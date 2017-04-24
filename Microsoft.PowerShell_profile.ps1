5f7af567e9b9a94f3f90c8cd1f04ce8c3aed89d9
rcmdnk <rcmdnk@gmail.com>
2017-04-25 03:00:36 +0900

    Microsoft.PowerShell_profile.ps1, from darwin16

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -34,3 +34,9 @@ function tail([string] $file, [int] $n=10, [switch] $f){
     Get-Content -Tail $n $file
   }
 }
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
