01a4d07e60a739f308001c6da52237521e8868ff
rcmdnk <rcmdnk@gmail.com>
2018-04-16 18:15:30 +0900

    install.sh    submodules/vim_ahk (new commits), from darwin17

--- install.sh
+++ install.sh
@@ -15,8 +15,9 @@ dryrun=0
 newlink=()
 exist=()
 curdir=$(pwd -P)
+
 # help
-HELP="Usage: $0 [-nd] [-b <backup file postfix>] [-e <exclude file>]
+HELP="Usage: $0 [-ndsh] [-b <backup file postfix>] [-e <exclude file>]
 
 Make links of windows related files/directories
 
@@ -25,14 +26,16 @@ Arguments:
       -e  Set additional exclude file (default: ${exclude[*]})
       -n  Don't overwrite if file is already exist
       -d  Dry run, don't install anything
+      -s  Use 'pwd' instead of 'pwd -P' to make a symbolic link
       -h  Print Help (this message) and exit
 "
-while getopts b:e:ndh OPT;do
+while getopts b:e:ndsh OPT;do
   case $OPT in
     "b" ) backup=$OPTARG ;;
-    "e" ) exclude=(${exclude[@]} "$OPTARG") ;;
+    "e" ) exclude=("${exclude[@]}" "$OPTARG") ;;
     "n" ) overwrite=0 ;;
     "d" ) dryrun=1 ;;
+    "s" ) curdir=$(pwd) ;;
     "h" ) echo "$HELP" 1>&2; exit ;;
     * ) echo "$HELP" 1>&2; exit 1;;
   esac
